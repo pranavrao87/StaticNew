0406f3e93fc1292691472a605856a791931ab9d7
pranavrao87 <108501622+pranavrao87@users.noreply.github.com>
2022-11-07 16:38:01 -0600

    Create TeleOp.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -0,0 +1,99 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+@TeleOp(name="TeleOPStol")
+public class TeleOPStol extends OpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotorEx leftFront, leftBack, rightFront, rightBack;
+    private boolean direction, togglePrecision;
+    private double factor;
+    boolean reverse;
+    final int balls = 3;
+    EasyToggle toggleA = new EasyToggle("a", false, 1, false, false);
+
+    @Override
+    public void init() {
+        telemetry.addData("init start", android.R.bool::new);
+        telemetry.update();
+        leftFront = (DcMotorEx) hardwareMap.dcMotor.get("FL");
+        leftBack = (DcMotorEx) hardwareMap.dcMotor.get("BL");
+        rightFront = (DcMotorEx) hardwareMap.dcMotor.get("FR");
+        rightBack = (DcMotorEx) hardwareMap.dcMotor.get("FL");
+        leftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addData("init end", android.R.bool::new);
+        telemetry.update();
+
+    }
+    @Override
+    public void loop(){
+        telemetry.addData("loop start", android.R.bool::new);
+        telemetry.update();
+        toggleA.updateStart(gamepad1.a);
+        //toggles precision mode if the right stick button is pressed
+        if (gamepad1.left_stick_button)
+            togglePrecision = true;
+        else if (gamepad1.right_stick_button)
+            togglePrecision = false;
+
+        //sets the factor multiplied to the power of the motors
+        factor = togglePrecision ? .3 : 1; //the power is 1/5th of its normal value while in precision mode
+
+        // Do not mess with this, if it works, it works
+        double x = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+        double stickAngle = Math.atan2(direction ? -gamepad1.left_stick_y : gamepad1.left_stick_y, direction ? gamepad1.left_stick_x : -gamepad1.left_stick_x); // desired robot angle from the angle of stick
+        double powerAngle = stickAngle - (Math.PI / 4); // conversion for correct power values
+        double rightX = gamepad1.right_stick_x; // right stick x axis controls turning
+        final double leftFrontPower = Range.clip(x * Math.cos(powerAngle) - rightX, -1.0, 1.0);
+        final double leftRearPower = Range.clip(x * Math.sin(powerAngle) - rightX, -1.0, 1.0);
+        final double rightFrontPower = Range.clip(x * Math.sin(powerAngle) + rightX, -1.0, 1.0);
+        final double rightRearPower = Range.clip(x * Math.cos(powerAngle) + rightX, -1.0, 1.0);
+
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        leftBack.setDirection(DcMotor.Direction.FORWARD);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        rightBack.setDirection(DcMotor.Direction.REVERSE);
+        leftFront.setPower(leftFrontPower * factor);
+        leftBack.setPower(leftRearPower * factor);
+        rightFront.setPower(rightFrontPower * factor);
+        rightBack.setPower(rightRearPower * factor);
+
+        telemetry.addData("loop end", android.R.bool::new);
+        telemetry.update();
+        toggleA.updateEnd();
+    }
+
+
+// heading to brazil
+// also doing your mom imo
+    // bruh
+    // wait
+
+
+}
